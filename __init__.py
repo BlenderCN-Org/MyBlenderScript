020ed218fda9bdf329ecdc44e383fe2f6e1246fe
Masataka SUMI <santarh@gmail.com>
2017-02-25 01:09:27 +0900

    Bone Constraint のミラーコピー

--- __init__.py
+++ __init__.py
@@ -8,12 +8,15 @@ import bpy
 import os
 
 from . import bone_constraints
+from . import mirror_bone_constraints
 
 def register():
     bpy.utils.register_class(bone_constraints.TheOthersBoneLocationConstraints)
+    bpy.utils.register_class(mirror_bone_constraints.MirrorBoneConstraints)
 
 def unregister():
     bpy.utils.unregister_class(bone_constraints.TheOthersBoneLocationConstraints)
+    bpy.utils.register_class(mirror_bone_constraints.MirrorBoneConstraints)
 
 if __name__ == "__main__":
     register()
